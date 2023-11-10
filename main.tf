dc13256c2b35dbb82c1ee3f830a3d90726699c6d
Desislav Arashev <desislav.arashev@daiteap.com>
2023-11-10 15:38:29 +0200

    Update nginx

--- main.tf
+++ main.tf
@@ -119,13 +119,13 @@ provider "helm" {
   }
 }
 
-resource "helm_release" "nginx-ingres" {
-  name       = "nginx-ingress"
+resource "helm_release" "ingress-nginx" {
+  name       = "ingress-nginx"
 
-  repository = "https://helm.nginx.com/stable"
-  chart      = "nginx-ingress"
-  namespace = "nginx-ingress"
-  version    = "0.17.1"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  namespace = "ingress-nginx"
+  version    = "4.7.1"
 
   create_namespace = true
 
