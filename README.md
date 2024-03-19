0a67eae83e6c906675adfbd7825902ce475daaf5
Karol Bialas <>
2024-03-19 03:56:21 +0100

    Add 'ingresspool' nodepool to terraform config Add ingress controller config to instruction

--- README.md
+++ README.md
@@ -34,15 +34,10 @@ To create the necessary infrastructure run the script ```./create-ionos-kubernet
 ```sh
 source .env && ./create-ionos-kubernetes.sh
 ```
-### 1.1 Create additional node pool
 
-    ionosctl datacenter list
-    export DE_DATACENTER_ID=0bbfc15f-32c5-48a8-a5e0-6a2d24378743
-    ionosctl k8s cluster list
-    export DE_CLUSTER_ID=bbbb0a6d-4158-40d6-8a96-a497bad459d3
-    ionosctl k8s cluster get --cluster-id $DE_CLUSTER_ID
-    export DOME_K8S_INGRESS_NODEPOOL_ID=$(ionosctl k8s nodepool create --cluster-id $DE_CLUSTER_ID --name federated-catalog-ingress-pool --node-count 1 --datacenter-id $DE_DATACENTER_ID --cpu-family "INTEL_SKYLAKE" --labels nodepool=ingress -o json | jq -r '.items[0].id')
+### 1.1 Install ingress-controller
 
+    helm install --replace -n nginx-ingress nginx-ingress ingress-nginx/ingress-nginx -f helm/ingress-controller/values.yaml --version 4.7.2 --create-namespace
 
 ### 2. cert-manager (optional)
 
--- main.tf
+++ main.tf
@@ -112,6 +112,35 @@ resource "ionoscloud_k8s_node_pool" "pool2" {
   timeouts {}
 }
 
+resource "ionoscloud_k8s_node_pool" "ingresspool" {
+  datacenter_id  = ionoscloud_datacenter.digital_ecosystems.id
+  k8s_cluster_id = ionoscloud_k8s_cluster.kubernetes1.id
+  name           = "federated-catalog-ingress-pool"
+  k8s_version    = ionoscloud_k8s_cluster.kubernetes1.k8s_version
+  maintenance_window {
+    day_of_the_week = "Sunday"
+    time            = "03:01:04Z"
+  }
+
+  dynamic "lans" {
+    for_each = var.create_private_lan ? [1] : []
+    content {
+      id   = ionoscloud_lan.lan1[0].id
+      dhcp = true
+    }
+  }
+  cpu_family        = "INTEL_SKYLAKE"
+  availability_zone = "AUTO"
+  storage_type      = "HDD"
+  node_count        = 1
+  cores_count       = var.cores_count
+  ram_size          = var.node_memory
+  storage_size      = 20
+  timeouts {}
+  labels            = {
+    nodepool             = "ingress"
+  }
+}
 
 ## configure access to the Container Registry in the K8S cluster
 data "ionoscloud_k8s_cluster" "kubernetes1" {
@@ -132,20 +161,3 @@ provider "helm" {
     config_path    = "kubeconfig-ionos.yaml"
   }
 }
-
-#resource "helm_release" "nginx-ingress" {
-#  name       = "nginx-ingress"
-#
-#  repository = "https://kubernetes.github.io/ingress-nginx"
-#  chart      = "ingress-nginx"
-#  namespace = "nginx-ingress"
-#  version    = "4.7.2"
-#
-#  create_namespace = true
-#
-#  timeout = 600
-#
-#  depends_on = [
-#    local_sensitive_file.kubeconfig
-#  ]
-#}
